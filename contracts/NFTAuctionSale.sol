fb5bae19c83bda90e05858a6d2180ecb8b2d6bf5
furysheep <furysheep@yahoo.com>
2021-02-23 00:42:59 +0800

    Refactored for gas usage, emergency stop and setters for auction struct

--- contracts/NFTAuctionSale.sol
+++ contracts/NFTAuctionSale.sol
@@ -46,15 +46,12 @@ contract NFTAuctionSale is Ownable {
     }
 
     bool public emergencyStop = false;
-    address public salesPerson;
 
     mapping(uint256 => Auction) public auctions;
 
     uint256 public indexOfAuction = 0;
 
-    constructor() public {
-        salesPerson = owner();
-    }
+    constructor() public {}
 
     function max(uint256 a, uint256 b) private pure returns (uint256) {
         return a > b ? a : b;
@@ -96,12 +93,14 @@ contract NFTAuctionSale is Ownable {
 
     /// @notice Transfers ERC20 tokens holding in contract to the contract owner
     /// @param tokenAddr ERC20 token address
-    function claimERC20Tokens(address tokenAddr) public onlyOwner {
-        IERC20(tokenAddr).transferFrom(
-            address(this),
-            owner(),
-            IERC20(tokenAddr).balanceOf(address(this))
-        );
+    function transferERC20(address tokenAddr) external onlyOwner {
+        IERC20 erc20 = IERC20(tokenAddr);
+        erc20.transfer(_msgSender(), erc20.balanceOf(address(this)));
+    }
+
+    /// @notice Transfers ETH holding in contract to the contract owner
+    function transferETH() external onlyOwner {
+        _msgSender().transfer(address(this).balance);
     }
 
     /// @notice Create auction with specific parameters
@@ -118,8 +117,9 @@ contract NFTAuctionSale is Ownable {
         uint256 totalSupply,
         uint256 startTime,
         uint256 endTime
-    ) public onlyOwner {
-        require(totalSupply > 0, "Total supply is 0");
+    ) external onlyOwner {
+        require(emergencyStop == false, "Emergency stopped");
+        require(totalSupply > 0, "Total supply should be greater than 0");
         IERC1155 auctionToken = IERC1155(auctionItemAddress);
 
         // check if the input address is ERC1155
@@ -156,6 +156,7 @@ contract NFTAuctionSale is Ownable {
     /// @notice Claim auction reward tokens to the caller
     /// @param auctionId Auction Id
     function claimReward(uint256 auctionId) external {
+        require(emergencyStop == false, "Emergency stopped");
         require(auctionId <= indexOfAuction, "Auction id is invalid");
 
         require(
@@ -183,7 +184,8 @@ contract NFTAuctionSale is Ownable {
     /// @notice Increase the caller's bid price
     /// @param auctionId Auction Id
     /// @param increaseAmount The incrementing price than the original bid
-    function increaseMyBid(uint256 auctionId, uint256 increaseAmount) public {
+    function increaseMyBid(uint256 auctionId, uint256 increaseAmount) external {
+        require(emergencyStop == false, "Emergency stopped");
         require(auctionId <= indexOfAuction, "Auction id is invalid");
         require(increaseAmount > 0, "Wrong amount");
         require(
@@ -218,7 +220,7 @@ contract NFTAuctionSale is Ownable {
     /// @param auctionId Auction Id
     /// @param bidPrice ERC20 token amount
     function makeBid(uint256 auctionId, uint256 bidPrice)
-        public
+        external
         isBidAvailable(auctionId)
     {
         uint256 minIndex = 0;
@@ -292,7 +294,7 @@ contract NFTAuctionSale is Ownable {
     /// @notice Check the auction is finished
     /// @param auctionId Auction Id
     /// @return bool true if finished, otherwise false
-    function isAuctionFinished(uint256 auctionId) public view returns (bool) {
+    function isAuctionFinished(uint256 auctionId) external view returns (bool) {
         require(auctionId <= indexOfAuction, "Invalid auction id");
         return (emergencyStop || auctions[auctionId].endTime < block.timestamp);
     }
@@ -300,38 +302,114 @@ contract NFTAuctionSale is Ownable {
     /// @notice Get remaining time for the auction
     /// @param auctionId Auction Id
     /// @return uint the remaining time for the auction
-    function getTimeRemaining(uint256 auctionId) public view returns (uint256) {
+    function getTimeRemaining(uint256 auctionId)
+        external
+        view
+        returns (uint256)
+    {
         require(auctionId <= indexOfAuction, "Invalid auction id");
         return auctions[auctionId].endTime - block.timestamp;
     }
 
     /// @notice Start emergency, only owner action
-    function setEmergencyStart() public onlyOwner {
+    function setEmergencyStart() external onlyOwner {
         emergencyStop = true;
         emit EmergencyStarted();
     }
 
     /// @notice Stop emergency, only owner action
-    function setEmergencyStop() public onlyOwner {
+    function setEmergencyStop() external onlyOwner {
         emergencyStop = false;
         emit EmergencyStopped();
+    }
+
+    /// @notice Change start time for auction
+    /// @param auctionId Auction Id
+    /// @param startTime new start time
+    function setStartTimeForAuction(uint256 auctionId, uint256 startTime)
+        external
+        onlyOwner
+    {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].startTime = startTime;
+    }
+
+    /// @notice Change end time for auction
+    /// @param auctionId Auction Id
+    /// @param endTime new end time
+    function setEndTimeForAuction(uint256 auctionId, uint256 endTime)
+        external
+        onlyOwner
+    {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].endTime = endTime;
+    }
+
+    /// @notice Change total supply for auction
+    /// @param auctionId Auction Id
+    /// @param totalSupply new Total supply
+    function setTotalSupplyForAuction(uint256 auctionId, uint256 totalSupply)
+        external
+        onlyOwner
+    {
+        require(totalSupply > 0, "Total supply should be greater than 0");
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].totalSupply = totalSupply;
+    }
+
+    /// @notice Change start price for auction
+    /// @param auctionId Auction Id
+    /// @param startPrice new Total supply
+    function setStartPriceForAuction(uint256 auctionId, uint256 startPrice)
+        external
+        onlyOwner
+    {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].startPrice = startPrice;
     }
 
     /// @notice Change max bid per wallet for auction, default is 1
     /// @param auctionId Auction Id
     /// @param maxBidPerWallet Max number of bids to set
-    function setMaxBidPerWallet(uint256 auctionId, uint256 maxBidPerWallet)
-        public
-        onlyOwner
-    {
+    function setMaxBidPerWalletForAuction(
+        uint256 auctionId,
+        uint256 maxBidPerWallet
+    ) external onlyOwner {
         require(auctionId <= indexOfAuction, "Invalid auction id");
         require(maxBidPerWallet > 0, "Should be greater than 0");
         auctions[auctionId].maxBidPerWallet = maxBidPerWallet;
     }
 
-    /// @notice Set sales person address
-    /// @param _salesPerson New sales person address
-    function setSalesPerson(address _salesPerson) public onlyOwner {
-        salesPerson = _salesPerson;
+    /// @notice Change ERC20 token address for auction
+    /// @param auctionId Auction Id
+    /// @param paymentTokenAddress new ERC20 token address
+    function setPaymentTokenAddressForAuction(
+        uint256 auctionId,
+        address paymentTokenAddress
+    ) external onlyOwner {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].paymentTokenAddress = paymentTokenAddress;
+    }
+
+    /// @notice Change auction item address for auction
+    /// @param auctionId Auction Id
+    /// @param auctionItemAddress new Auctioned item address
+    function setAuctionItemAddress(
+        uint256 auctionId,
+        address auctionItemAddress
+    ) external onlyOwner {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].auctionItemAddress = auctionItemAddress;
+    }
+
+    /// @notice Change auction item token id
+    /// @param auctionId Auction Id
+    /// @param auctionItemTokenId new token id
+    function setAuctionItemTokenId(
+        uint256 auctionId,
+        uint256 auctionItemTokenId
+    ) external onlyOwner {
+        require(auctionId <= indexOfAuction, "Invalid auction id");
+        auctions[auctionId].auctionItemTokenId = auctionItemTokenId;
     }
 }
